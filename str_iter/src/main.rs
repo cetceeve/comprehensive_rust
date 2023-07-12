e1e9fa789e780b9170d8ffaefa932efca4b2a3e0
Fabian Zeiher <fzeiher@gmail.com>
2023-07-12 09:50:08 +0900

    finished day 2 afternoon exercise strings and iterators

---
+++ str_iter/src/main.rs
@@ -0,0 +1,47 @@
+pub fn prefix_matches(prefix: &str, request_path: &str) -> bool {
+    let mut req_segments = request_path.split("/");
+    for prefix_segment in prefix.split("/") {
+        let Some(req_segment) = req_segments.next() else { return false; };
+        if prefix_segment == "*" {
+            continue;
+        }
+        if prefix_segment != req_segment { return false; }
+    }
+    true
+}
+
+#[test]
+fn test_matches_without_wildcard() {
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc-123"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc/books"));
+
+    assert!(!prefix_matches("/v1/publishers", "/v1"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/publishersBooks"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/parent/publishers"));
+}
+
+#[test]
+fn test_matches_with_wildcard() {
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/bar/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books/book1"
+    ));
+
+    assert!(!prefix_matches("/v1/publishers/*/books", "/v1/publishers"));
+    assert!(!prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/booksByAuthor"
+    ));
+}
+
+#[allow(dead_code)]
+fn main() {}
